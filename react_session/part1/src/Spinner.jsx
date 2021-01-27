6841b05e1d0a91405e12558cfd6c62104389a69a
Volodymyr Meduna <liodik89@gmail.com>
2021-01-27 12:10:31 +0200

    commit

---
+++ react_session/part1/src/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// input num
+//
+const Spinner = ({ size }) => {
+  return <div className="spinner" style={{ height: size, width: size }}></div>;
+};
+
+export default Spinner;
