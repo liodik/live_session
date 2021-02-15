aecbfb75aa8aade538de906ea191ef7c286b3bf0
Volodymyr Meduna <liodik89@gmail.com>
2021-02-15 12:55:54 +0200

    commit

--- react_session/route/task1/src/User.jsx
+++ react_session/route/task1/src/User.jsx
@@ -1,58 +1,56 @@
-import React, { Component } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 // algo
 // 1. create empty state
 // 2. make http call, componentDidMount && update state
 // 3.  handle UserID
 
-class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      avatar: null,
-      name: null,
-      location: null,
-    };
-  }
-  getUserData = userId => {
+const User = () => {
+  // input: InitState
+  // output: array (stateValue, func to update state)
+
+  const [userInfo, setUserInfo] = useState({
+    avatar: null,
+    name: null,
+    location: null,
+  });
+  const { userId } = useParams();
+  useEffect(() => {
+    // useEffect
+    // input: func, arr
+    // output: undefined
+    console.log('componentDidMount');
     fetch(`https://api.github.com/users/${userId}`)
       .then(response => response.json())
       .then(userData => {
         const { avatar_url, name, location } = userData;
-        this.setState({
+        setUserInfo({
           avatar: avatar_url,
           name,
           location,
         });
       });
-
-    // TODO make error handler
-  };
-
-  componentDidMount() {
-    this.getUserData(this.props.match.params.userId);
+    return () => {
+      //componentWillUnmount
+      console.log('componentWillUnmount');
+    };
+  }, [userId]);
+  // callback
+  // input
+  // output: undefined
+  const { avatar, name, location } = userInfo;
+  if (!avatar || !name || !location) {
+    return null;
   }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.match.params.userId !== prevProps.match.params.userId) {
-      this.getUserData(this.props.match.params.userId);
-    }
-  }
-  render() {
-    const { avatar, name, location } = this.state;
-    if (!avatar || !name || !location) {
-      return null;
-    }
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatar} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{name}</span>
-          <span className="user__location">{location}</span>
-        </div>
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default User;
